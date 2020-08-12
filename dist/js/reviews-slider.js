3f319e2a3311607368c6ddc8fba6cb7486c26f15
oksder <oksana.dergacova@gmail.com>
2020-08-12 20:07:55 +0100

    Added js for slider buttons

--- dist/js/reviews-slider.js
+++ dist/js/reviews-slider.js
@@ -46,3 +46,16 @@ sliderToggles.forEach((sliderToggle) => {
     bindClickReviews(i);
   });
 });
+
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
