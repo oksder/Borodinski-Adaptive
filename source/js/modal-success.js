ba387746f55ccdf188e1d73c89fc32f84b762a93
oksder <oksana.dergacova@gmail.com>
2020-09-25 17:48:44 +0100

    Eddited JS for modal-success

--- source/js/modal-success.js
+++ source/js/modal-success.js
@@ -1,12 +1,12 @@
-var link = document.querySelector(".other-button__button");
-var popup = document.querySelector(".modal-success");
-var popupClose = document.querySelector(".modal-success__button");
+var open = document.querySelector(".other-button__button");
+var modalPopup = document.querySelector(".modal-success");
+var modalClose = document.querySelector(".modal-success__button");
 
-link.addEventListener("click", function (evt) {
+open.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.add("modal-show");
+  modalPopup.classList.add("modal-success-show");
 });
 
-popupClose.addEventListener("click", function () {
-  popup.classList.remove("modal-show");
+modalClose.addEventListener("click", function () {
+  modalPopup.classList.remove("modal-success-show");
 });
